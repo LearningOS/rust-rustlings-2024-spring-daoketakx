fab19ce63632f701d900d3855412923d84c62b16
muzhen <3513442042@qq.com>
2024-04-25 17:38:15 +0800

    update

--- exercises/algorithm/algorithm1.rs
+++ exercises/algorithm/algorithm1.rs
@@ -2,7 +2,7 @@
 	single linked list merge
 	This problem requires you to merge two ordered singly linked lists into one ordered singly linked list
 */
-// I AM NOT DONE
+
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
@@ -29,13 +29,13 @@ struct LinkedList<T> {
     end: Option<NonNull<Node<T>>>,
 }
 
-impl<T> Default for LinkedList<T> {
+impl<T: std::cmp::PartialOrd + Clone> Default for LinkedList<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl<T> LinkedList<T> {
+impl<T: std::cmp::PartialOrd + Clone> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             length: 0,
@@ -72,11 +72,34 @@ impl<T> LinkedList<T> {
 	pub fn merge(list_a:LinkedList<T>,list_b:LinkedList<T>) -> Self
 	{
 		//TODO
-		Self {
-            length: 0,
-            start: None,
-            end: None,
+        let mut merge_list = LinkedList::new();
+        let mut ptr_a = list_a.start;
+        let mut ptr_b = list_b.start;
+
+        while ptr_a.is_some() && ptr_b.is_some(){
+            
+            if unsafe{ptr_a.unwrap().as_ref().val.clone()} <= unsafe{ptr_b.unwrap().as_ref().val.clone()}{
+                merge_list.add(unsafe{ptr_a.unwrap().as_ref().val.clone()});
+                ptr_a = unsafe{ptr_a.unwrap().as_ref().next};
+            }else{
+                merge_list.add(unsafe{ptr_b.unwrap().as_ref().val.clone()});
+                ptr_b = unsafe{ptr_b.unwrap().as_ref().next};
+            }
+
         }
+
+        if ptr_a.is_some(){
+            while ptr_a.is_some(){
+                merge_list.add(unsafe{ptr_a.unwrap().as_ref().val.clone()});
+                ptr_a = unsafe{ptr_a.unwrap().as_ref().next};
+            }
+        }else{
+            while ptr_b.is_some(){
+                merge_list.add(unsafe{ptr_b.unwrap().as_ref().val.clone()});
+                ptr_b = unsafe{ptr_b.unwrap().as_ref().next};
+            }
+        }
+        merge_list
 	}
 }
 
